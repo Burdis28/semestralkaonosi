2ed74c68b0d3b487645a1f79566c32a0c018dccf
Ondrej Burda <ondrej.burda@plus4u.net>
2020-05-12 13:54:27 +0200

    more fixes

--- src/main/java/upce/sem/semestralkabe/schema/Toy.java
+++ src/main/java/upce/sem/semestralkabe/schema/Toy.java
@@ -1,19 +1,14 @@
 package upce.sem.semestralkabe.schema;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +20,8 @@ public class Toy {
   private Long id;
 
   private String name;
+
+  @Column(columnDefinition = "LONGTEXT")
   private String image;
 
   @ManyToOne(fetch = FetchType.LAZY)
